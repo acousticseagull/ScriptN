96b044307d71f7687a8e391ea332c5a8011fc4fa
acousticseagull <acousticseagull@gmail.com>
2022-11-12 22:36:55 -0500

    1 file modified

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -1,41 +1,47 @@
 import tag from '../tag';
+import {
+  createScene,
+  createAction,
+  createCharacter,
+  createDialog,
+} from '../create';
 
 const save = () => {
-  localStorage.setItem(
-    'script',
-    JSON.stringify({
-      title: document.querySelector('.title').innerText,
-      content: Array.from(document.querySelectorAll('.scene')).map((scene) => ({
-        heading: scene.querySelector('.heading').innerText,
-        blocks: Array.from(
-          document.querySelectorAll('.action, .character, .dialog')
-        ).map(({ className, innerText }) => ({
-          type: className,
-          content: innerText,
-        })),
+  const script = JSON.stringify({
+    title: document.querySelector('.title').innerText,
+    content: Array.from(document.querySelectorAll('.scene')).map((scene) => ({
+      heading: scene.querySelector('.heading').innerText,
+      blocks: Array.from(
+        document.querySelectorAll('.action, .character, .dialog')
+      ).map(({ className, innerText }) => ({
+        type: className,
+        content: innerText,
       })),
-    })
-  );
+    })),
+  });
+
+  console.log(script);
+
+  localStorage.setItem('script', script);
 };
 
-const load = () => {
+const load = (target) => {
   const script = JSON.parse(localStorage.getItem('script'));
-  document.querySelector('.toolbar .title').innerText = script.title;
 
   document.querySelectorAll('.scene').forEach((item) => item.remove());
 
-  const fragment = document.createDocumentFragment();
+  document.querySelector('.toolbar > .title').innerText = script.title;
 
-  script.content.forEach((scene) => {
-    fragment.append(
-      createScene(scene.heading).append(
-        scene.blocks.map(({ type, content }) => {
-          if (type === 'action') return createAction(content);
-          if (type === 'character') return createCharacter(content);
-          if (type === 'dialog') return createDialog(content);
-        })
-      )
-    );
+  script.content.forEach(({ heading, blocks }) => {
+    const scene = createScene(heading);
+
+    if (blocks.length) scene.append(...blocks.map(({ type, content }) => {
+      if (type === 'action') return createAction(content);
+      if (type === 'character') return createCharacter(content);
+      if (type === 'dialog') return createDialog(content);
+    }));
+
+    target.append(scene);
   });
 };
 
@@ -44,7 +50,7 @@ export const toolbar = (target) => {
     tag('div', { class: 'toolbar' }, [
       tag('button', { onclick: save }, 'Save'),
       tag('div', { class: 'title', contenteditable: true }, 'Untitled'),
-      tag('button', 'Load'),
+      tag('button', { onclick: () => load(target) }, 'Load'),
     ])
   );
 };
