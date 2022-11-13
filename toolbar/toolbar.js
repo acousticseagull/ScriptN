e014ab9125e68fc70228ad136df95bc7313c0880
acousticseagull <acousticseagull@gmail.com>
2022-11-12 22:09:34 -0500

    1 file modified

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -1,29 +1,42 @@
 import tag from '../tag';
 
 const save = () => {
-  localStorage.setItem('script', JSON.stringify({
-    title: document.querySelector('.title').innerText,
-    content: Array.from(document.querySelectorAll('.scene')).map((scene) => ({
-      heading: scene.querySelector('.heading').innerText,
-      blocks: Array.from(
-        document.querySelectorAll('.action, .character, .dialog')
-      ).map(({ className, innerText }) => ({
-        type: className,
-        content: innerText,
+  localStorage.setItem(
+    'script',
+    JSON.stringify({
+      title: document.querySelector('.title').innerText,
+      content: Array.from(document.querySelectorAll('.scene')).map((scene) => ({
+        heading: scene.querySelector('.heading').innerText,
+        blocks: Array.from(
+          document.querySelectorAll('.action, .character, .dialog')
+        ).map(({ className, innerText }) => ({
+          type: className,
+          content: innerText,
+        })),
       })),
-    })),
-  }));
+    })
+  );
 };
 
 const load = () => {
   const script = JSON.parse(localStorage.getItem('script'));
   document.querySelector('.toolbar .title').innerText = script.title;
 
-  document.querySelectorAll('.scene').forEach(item => item.remove());
+  document.querySelectorAll('.scene').forEach((item) => item.remove());
 
-  script.content.forEach(scene => {
+  const fragment = document.createDocumentFragment();
 
-  })
+  script.content.forEach((scene) => {
+    fragment.append(
+      createScene(scene.heading).append(
+        scene.blocks.map(({ type, content }) => {
+          if (type === 'action') return createAction(content);
+          if (type === 'character') return createCharacter(content);
+          if (type === 'dialog') return createDialog(content);
+        })
+      )
+    );
+  });
 };
 
 export const toolbar = (target) => {
