bb28eee371da48b4b6ab58fbc36533697ae59a39
acousticseagull <acousticseagull@gmail.com>
2022-11-12 21:50:02 -0500

    1 file modified

--- toolbar/toolbar.js
+++ toolbar/toolbar.js
@@ -1,9 +1,27 @@
 import tag from '../tag';
 
+const save = () => {
+  const script = Array.from(document.querySelectorAll('.scene')).map(
+    (scene) => ({
+      heading: scene.querySelector('.heading').innerText,
+      blocks: Array.from(
+        document.querySelectorAll('.action, .character, .dialog')
+      ).map(({ className, innerText }) => ({
+        type: className,
+        content: innerText,
+      })),
+    })
+  );
+
+  localStorage.setItem('script', script);
+};
+
+const load = () => {};
+
 export const toolbar = (target) => {
   target.append(
     tag('div', { class: 'toolbar' }, [
-      tag('button', 'Save'),
+      tag('button', { onclick: save }, 'Save'),
       tag('div', { class: 'title', contenteditable: true }, 'Untitled'),
       tag('button', 'Load'),
     ])
