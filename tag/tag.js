14bc43cd9821b0ee618debe01e1246daab97ed3c
acousticseagull <acousticseagull@gmail.com>
2022-12-09 22:21:35 -0500

    1 file modified

--- tag/tag.js
+++ tag/tag.js
@@ -1,23 +1,21 @@
 export default function tag(...args) {
   let node;
 
-  const compose = function (item, i) {
-    if (i == 0) {
+  function compose(item, i) {
+    if (i === 0) {
       node = document.createElement(item);
-      if (item == 'a') node.setAttribute('href', '#');
-    } else if (typeof item == 'string' || typeof item == 'number') {
+      if (item === 'a') node.setAttribute('href', '#');
+    } else if (['string', 'number'].includes(typeof item)) {
       node.appendChild(document.createTextNode(item));
     } else if (item.tagName) {
       node.appendChild(item);
     } else if (Array.isArray(item)) {
-      for (var i = 0, l = item.length; i < l; i++) {
-        compose(item[i]);
-      }
+      for (let i = 0, l = item.length; i < l; i++) compose(item[i]);
     } else {
-      for (var p in item) {
-        if (/^on/.test(p)) {
+      for (let p in item) {
+        if (p.startsWith('on')) {
           node.addEventListener(p.substring(2, p.length), item[p]);
-        } else if (typeof item[p] == 'boolean') {
+        } else if (typeof item[p] === 'boolean') {
           if (item[p]) node.setAttribute(p, '');
         } else {
           node.setAttribute(p, item[p]);
@@ -26,9 +24,7 @@ export default function tag(...args) {
     }
   };
 
-  for (var i = 0, l = args.length; i < l; i++) {
-    compose(args[i], i);
-  }
+  for (let i = 0, l = args.length; i < l; i++) compose(args[i], i);
 
   return node;
 }
